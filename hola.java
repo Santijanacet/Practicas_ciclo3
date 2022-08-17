816f04403c165505b15c698d9e6f5002d63b9e75
Santiago Janacet <santijanacet06@gmail.com>
2022-08-16 20:28:43 -0500

    estoy probando

---
+++ hola.java
@@ -0,0 +1,17 @@
+public class hola {
+    public static void main(String[] args) {
+        System.out.println("hola mundo");
+        int a = 10;
+        int b = 19;
+
+        if(a == b){
+            System.out.println("estos numeros son iguales ");
+        }else {
+
+            System.out.println("los numeros son diferentes");
+        }
+    }
+
+
+
+}
